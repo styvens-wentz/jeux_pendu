ed7b7f271a2e9461dc4a1f09e5e5a8142a27541e
styvens59 <56022040+styvens59@users.noreply.github.com>
2019-12-03 14:51:37 +0100

    V1: jeux du pendu fini avec 1 seul mode de difficulté

--- base.js
+++ base.js
@@ -1,65 +1,65 @@
 const mots = ['ANGLE', 'ARMOIRE', 'BANC', 'BUREAU', 'CABINET', 'CARREAU', 'CHAISE', 'CLASSE', 'CLEF', 'COIN', 'COULOIR', 'DOSSIER', 'EAU', 'ECOLE', 'ENTRER', 'ESCALIER', 'ETAGERE', 'EXTERIEUR', 'FENETRE', 'INTERIEUR', 'LAVABO', 'LIT', 'MARCHE', 'MATELAS', 'MATERNELLE', 'MEUBLE', 'MOUSSE', 'MUR', 'PELUCHE', 'PLACARD', 'PLAFOND', 'PORTE', 'POUBELLE', 'RADIATEUR', 'RAMPE', 'RIDEAU', 'ROBINET', 'SALLE', 'SALON', 'SERRURE', 'SERVIETTE', 'SIEGE', 'SIESTE', 'SILENCE', 'SOL', 'SOMMEIL', 'SONNETTE', 'SORTIE', 'TABLE', 'TABLEAU', 'TABOURET', 'TAPIS', 'TIROIR', 'TOILETTE', 'VITRE', 'ALLER', 'AMENER', 'APPORTER', 'APPUYER', 'ATTENDRE', 'BAILLER', 'COUCHER', 'DORMIR', 'ECLAIRER', 'EMMENER', 'EMPORTER', 'ENTRER', 'FERMER', 'FRAPPER', 'INSTALLER', 'LEVER', 'OUVRIR', 'PRESSER', 'RECHAUFFER', 'RESTER', 'SONNER', 'SORTIR', 'VENIR', 'ABSENT', 'ASSIS', 'BAS', 'HAUT', 'PRESENT', 'GAUCHE', 'DROITE', 'DEBOUT', 'DEDANS', 'DEHORS', 'FACE', 'LOIN', 'PRES', 'TARD', 'TOT', 'APRES', 'AVANT', 'CONTRE', 'DANS', 'DE', 'DERRIERE', 'DEVANT', 'DU', 'SOUS', 'SUR', 'CRAYON', 'STYLO', 'FEUTRE', 'MINE', 'GOMME', 'DESSIN', 'COLORIAGE', 'RAYURE', 'PEINTURE', 'PINCEAU', 'COULEUR', 'CRAIE', 'PAPIER', 'FEUILLE', 'CAHIER', 'CARNET', 'CARTON', 'CISEAUX', 'DECOUPAGE', 'PLIAGE', 'PLI', 'COLLE', 'AFFAIRE', 'BOITE', 'CASIER', 'CAISSE', 'TROUSSE', 'CARTABLE', 'JEU', 'JOUET', 'PION', 'DOMINO', 'PUZZLE', 'CUBE', 'PERLE', 'CHOSE', 'FORME', 'CARRE', 'ROND', 'PATE', 'MODELER', 'TAMPON', 'LIVRE', 'HISTOIRE', 'BIBLIOTHEQUE', 'IMAGE', 'ALBUM', 'TITRE', 'CONTE', 'DICTIONNAIRE', 'MAGAZINE', 'CATALOGUE', 'PAGE', 'LIGNE', 'MOT', 'ENVELOPPE', 'ETIQUETTE', 'CARTE', 'APPEL', 'AFFICHE', 'ALPHABET', 'APPAREIL', 'CAMESCOPE', 'CASSETTE', 'CHAINE', 'CHANSON', 'CHIFFRE', 'CONTRAIRE', 'DIFFERENCE', 'DOIGT', 'ECRAN', 'ECRITURE', 'FILM', 'FOIS', 'FOI', 'IDEE', 'INSTRUMENT', 'INTRUS', 'LETTRE', 'LISTE', 'MAGNETOSCOPE', 'MAIN', 'MICRO', 'MODELE', 'MUSIQUE', 'NOM', 'NOMBRE', 'ORCHESTRE', 'ORDINATEUR', 'PHOTO', 'POINT', 'POSTER', 'POUCE', 'PRENOM', 'QUESTION', 'RADIO', 'SENS', 'TAMBOUR', 'TELECOMMANDE', 'TELEPHONE', 'TELEVISION', 'TRAIT', 'TROMPETTE', 'VOIX', 'XYLOPHONE', 'ZERO', 'CHANTER', 'CHERCHER', 'CHOISIR', 'CHUCHOTER', 'COLLER', 'COLORIER', 'COMMENCER', 'COMPARER', 'COMPTER', 'CONSTRUIRE', 'CONTINUER', 'COPIER', 'COUPER', 'DECHIRER', 'DECOLLER', 'DECORER', 'DECOUPER', 'DEMOLIR', 'DESSINER', 'DIRE', 'DISCUTER', 'ECOUTER', 'ECRIRE', 'EFFACER', 'ENTENDRE', 'ENTOURER', 'ENVOYER', 'FAIRE', 'FINIR', 'FOUILLER', 'GOUTER', 'IMITER', 'LAISSER', 'LIRE', 'METTRE', 'MONTRER', 'OUVRIR', 'PARLER', 'PEINDRE', 'PLIER', 'POSER', 'PRENDRE', 'PREPARER', 'RANGER', 'RECITER', 'RECOMMENCER', 'REGARDER', 'REMETTRE', 'REPETER', 'REPONDRE', 'SENTIR', 'SOULIGNER', 'TAILLER', 'TENIR', 'TERMINER', 'TOUCHER', 'TRAVAILLER', 'TRIER', 'AMI', 'ATTENTION', 'CAMARADE', 'COLERE', 'COPAIN', 'COQUIN', 'DAME', 'DIRECTEUR', 'DIRECTRICE', 'DROIT', 'EFFORT', 'ELEVE', 'ENFANT', 'FATIGUE', 'FAUTE', 'FILLE', 'GARCON', 'GARDIEN', 'MADAME', 'MAITRE', 'MAITRESSE', 'MENSONGE', 'ORDRE', 'PERSONNE', 'RETARD', 'JOUEUR', 'SOURIRE', 'TRAVAIL', 'AIDER', 'DEFENDRE', 'DESOBEIR', 'DISTRIBUER', 'ECHANGER', 'EXPLIQUER', 'GRONDER', 'OBEIR', 'OBLIGER', 'PARTAGER', 'PRETER', 'PRIVER', 'PROMETTRE', 'PROGRES', 'PROGRESSER', 'PUNIR', 'QUITTER', 'RACONTER', 'EXPLIQUER', 'REFUSER', 'SEPARER', 'BLOND', 'BRUN', 'CALME', 'CURIEUX', 'DIFFERENT', 'DOUX', 'ENERVER', 'GENTIL', 'GRAND', 'HANDICAPE', 'INSEPARABLE', 'JALOUX', 'MOYEN', 'MUET', 'NOIR', 'NOUVEAU', 'PETIT', 'POLI', 'PROPRE', 'ROUX', 'SAGE', 'SALE', 'SERIEUX', 'SOURD', 'TRANQUILLE', 'ARROSOIR', 'ASSIETTE', 'BALLE', 'BATEAU', 'BOITE', 'BOUCHON', 'BOUTEILLE', 'BULLES', 'CANARD', 'CASSEROLE', 'CUILLERE', 'CUVETTE', 'DOUCHE', 'ENTONNOIR', 'GOUTTES', 'LITRE', 'MOULIN', 'PLUIE', 'POISSON', 'PONT', 'POT', 'ROUE', 'SAC', 'PLASTIQUE', 'SALADIER', 'SEAU', 'TABLIER', 'TASSE', 'TROUS', 'VERRE', 'AGITER', 'AMUSER', 'ARROSER', 'ATTRAPER', 'AVANCER', 'BAIGNER', 'BARBOTER', 'BOUCHER', 'BOUGER', 'DEBORDER', 'DOUCHER', 'ECLABOUSSER', 'ESSUYER', 'ENVOYER', 'COULER', 'PARTIR', 'FLOTTER', 'GONFLER', 'INONDER', 'JOUER', 'LAVER', 'MELANGER', 'MOUILLER', 'NAGER', 'PLEUVOIR', 'PLONGER', 'POUSSER', 'POUVOIR', 'PRESSER', 'RECEVOIR', 'REMPLIR', 'RENVERSER', 'SECHER', 'SERRER', 'SOUFFLER', 'TIRER', 'TOURNER', 'TREMPER', 'VERSER', 'VIDER', 'VOULOIR', 'AMUSANT', 'CHAUD', 'FROID', 'HUMIDE', 'INTERESSANT', 'MOUILLE', 'SEC', 'TRANSPARENT', 'MOITIE', 'AUTANT', 'BEAUCOUP', 'ENCORE', 'MOINS', 'PEU', 'PLUS', 'TROP', 'ANORAK', 'ARC', 'BAGAGE', 'BAGUETTE', 'BARBE', 'BONNET', 'BOTTE', 'BOUTON', 'BRETELLE', 'CAGOULE', 'CASQUE', 'CASQUETTE', 'CEINTURE', 'CHAPEAU', 'CHAUSSETTE', 'CHAUSSON', 'CHAUSSURE', 'CHEMISE', 'CIGARETTE', 'COL', 'COLLANT', 'COURONNE', 'CRAVATE', 'CULOTTE', 'ECHARPE', 'EPEE', 'FEE', 'FLECHE', 'FUSIL', 'GANT', 'HABIT', 'JEAN', 'JUPE', 'LACET', 'LAINE', 'LINGE', 'LUNETTES', 'MAGICIEN', 'MAGIE', 'MAILLOT', 'MANCHE', 'MANTEAU', 'MOUCHOIR', 'MOUFLE', 'NOEUD', 'PAIRE', 'PANTALON', 'PIED', 'POCHE', 'PRINCE', 'PYJAMA', 'REINE', 'ROBE', 'ROI', 'RUBAN', 'SEMELLE', 'SOLDAT', 'SOCIERE', 'TACHE', 'TAILLE', 'TALON', 'TISSU', 'TRICOT', 'UNIFORME', 'VALISE', 'VESTE', 'VETEMENT', 'CHANGER', 'CHAUSSER', 'COUVRIR', 'DEGUISER', 'DESHABILLER', 'ENLEVER', 'HABILLER', 'LACER', 'PORTER', 'RESSEMBLER', 'CLAIR', 'COURT', 'ETROIT', 'FONCE', 'JOLI', 'LARGE', 'LONG', 'MULTICOLORE', 'NU', 'USE', 'BIEN', 'MAL', 'MIEUX', 'PRESQUE', 'AIGUILLE', 'AMPOULE', 'AVION', 'BOIS', 'BOUT', 'BRICOLAGE', 'BRUIT', 'CABANE', 'CARTON', 'CLOU', 'COLLE', 'CROCHET', 'ELASTIQUE', 'FICELLE', 'FIL', 'MARIONNETTE', 'MARTEAU', 'METAL', 'METRE', 'MORCEAU', 'MOTEUR', 'OBJET', 'OUTIL', 'PEINTURE', 'PINCEAU', 'PLANCHE', 'PLATRE', 'SCIE', 'TOURNEVIS', 'VIS', 'VOITURE', 'ARRACHER', 'ATTACHER', 'CASSER', 'COUDRE', 'DETRUIRE', 'ECORCHER', 'ENFILER', 'ENFONCER', 'FABRIQUER', 'MESURER', 'PERCER', 'PINCER', 'REPARER', 'REUSSIR', 'SERVIR', 'TAPER', 'TROUER', 'TROUVER', 'ADROIT', 'DIFFICILE', 'DUR', 'FACILE', 'LISSE', 'MALADROIT', 'POINTU', 'TORDU', 'ACCIDENT', 'AEROPORT', 'CAMION', 'ENGIN', 'FEU', 'FREIN', 'FUSEE', 'GARAGE', 'GARE', 'GRUE', 'HELICOPTERE', 'MOTO', 'PANNE', 'PARKING', 'PILOTE', 'PNEU', 'QUAI', 'TRAIN', 'VIRAGE', 'VITESSE', 'VOYAGE', 'WAGON', 'ZIGZAG', 'ARRETER', 'ATTERRIR', 'BOUDER', 'CHARGER', 'CONDUIRE', 'DEMARRER', 'DISPARAITRE', 'DONNER', 'ECRASER', 'ENVOLER', 'GARDER', 'GARER', 'MANQUER', 'PARTIR', 'POSER', 'RECULER', 'ROULER', 'TENDRE', 'TRANSPORTER', 'VOLER', 'ABIME', 'ANCIEN', 'BLANC', 'BLEU', 'CASSE', 'CINQ', 'DERNIER', 'DEUX', 'DEUXIEME', 'DIX', 'GRIS', 'GROS', 'HUIT', 'JAUNE', 'MEME', 'NEUF', 'PAREIL', 'PREMIER', 'QUATRE', 'ROUGE', 'SEPT', 'SEUL', 'SIX', 'SOLIDE', 'TROIS', 'TROISIEME', 'UN', 'VERT', 'DESSUS', 'AUTOUR', 'VITE', 'VERS', 'ACROBATE', 'ARRET', 'ARRIERE', 'BARRE', 'BARREAU', 'BORD', 'BRAS', 'CERCEAU', 'CHAISE', 'CHEVILLE', 'CHUTE', 'COEUR', 'CORDE', 'CORPS', 'COTE', 'COU', 'COUDE', 'CUISSE', 'DANGER', 'DOIGTS', 'DOS', 'ECHASSES', 'ECHELLE', 'EPAULE', 'EQUIPE', 'ESCABEAU', 'FESSE', 'FILET', 'FOND', 'GENOU', 'GYMNASTIQUE', 'HANCHE', 'JAMBE', 'JEU', 'MAINS', 'MILIEU', 'MONTAGNE', 'MUR', 'ESCALADE', 'MUSCLE', 'NUMERO', 'ONGLE', 'PARCOURS', 'PAS', 'PASSERELLE', 'PENTE', 'PEUR', 'PIED', 'PLONGEOIR', 'POIGNET', 'POING', 'PONT', 'SIGNE', 'SINGE', 'POUTRE', 'EQUILIBRE', 'PRISE', 'RIVIERE', 'CROCODILE', 'ROULADE', 'PIROUETTE', 'SAUT', 'SERPENT', 'SPORT', 'SUIVANT', 'TETE', 'TOBOGGAN', 'TOUR', 'TRAMPOLINE', 'TUNNEL', 'VENTRE', 'ACCROCHER', 'APPUYER', 'ARRIVER', 'BAISSER', 'BALANCER', 'BONDIR', 'BOUSCULER', 'COGNER', 'COURIR', 'DANSER', 'DEPASSER', 'DESCENDRE', 'ECARTER', 'ESCALADER', 'GAGNER', 'GENER', 'GLISSER', 'GRIMPER', 'MARCHER', 'PATTES', 'DEBOUT', 'MONTER', 'MONTRER', 'PENCHER', 'PERCHER', 'PERDRE', 'RAMPER', 'RATER', 'REMPLACER', 'RESPIRER', 'RETOURNER', 'REVENIR', 'SAUTER', 'SOULEVER', 'SUIVRE', 'TOMBER', 'TRANSPIRER', 'TRAVERSER', 'DANGEUREUX', 'EPAIS', 'FORT', 'GROUPE', 'IMMOBILE', 'ROND', 'SERRE', 'SOUPLE', 'ENSEMBLE', 'ICI', 'JAMAIS', 'TOUJOURS', 'SOUVENT', 'BAGARRE', 'BALANCOIRE', 'BALLON', 'BANDE', 'BICYCLETTE', 'BILLE', 'CAGE', 'ECUREUIL', 'CERF', 'VOLANT', 'CHATEAU', 'COUP', 'COUR', 'COURSE', 'ECHASSE', 'FLAQUE', 'EAU', 'PAIX', 'PARDON', 'PARTIE', 'PEDALE', 'PELLE', 'POMPE', 'PREAU', 'RAQUETTE', 'RAYON', 'RECREATION', 'SABLE', 'SIFFLET', 'SIGNE', 'TAS', 'TRICYCLE', 'TUYAU', 'VELO', 'FILE', 'RANG', 'BAGARRER', 'BATTRE', 'CACHER', 'CRACHER', 'CREUSER', 'CRIER', 'DEGONFLER', 'DISPUTE', 'EMPECHER', 'GALOPER', 'HURLER', 'JONGLER', 'LANCER', 'PEDALER', 'PLAINDRE', 'PLEURER', 'POURSUIVRE', 'PROTEGER', 'SAIGNER', 'SALIR', 'SIFFLER', 'SURVEILLER', 'TRAINER', 'TROUVER', 'FOU', 'MECHANT'];
-const motsAleatoire = mots[Math.floor(Math.random() * mots.length)];
-//const valider = document.getElementById('valider');
-//const lettre_entrer = document.getElementById('lettre_entrer');
-const lettre_trouver = document.getElementById('lettre_trouver');
+let lettre_trouver = document.getElementById('lettre_trouver');
 let nombre_lettre = document.getElementById('nombre_lettre');
-const clavier = document.getElementById('clavier');
-let reponse;
+const tableauMot = [];
+let tailleMot;
+let coupsManques = 0;
+let lettresTrouvees = 0;
+let fini = false;
+let motSecret;
 
 
+motSecret=mots[Math.floor(Math.random() * mots.length)];
+tailleMot=motSecret.length;
+nombre_lettre.innerHTML = tailleMot;
 
-function initReponse(){
-    reponse = "" ;
-    nombre_lettre.innerHTML = motsAleatoire.length;
-    for (var i = 0 ; i < motsAleatoire.length ; i++) {
-        reponse += ' _ ' ;
-    }
-    lettre_trouver.innerHTML = reponse ;
+for(let i=0; i<tailleMot; i++) {
+    const td = document.createElement("td");
+    lettre_trouver.appendChild(td);
+    const p = document.createElement("p");
+    p.id = i;
+    p.innerHTML = motSecret.charAt(i);
+    td.appendChild(p)
 }
 
-initReponse();
-/*valider.addEventListener("click", function () {
-    for (let i = 0; i < motsAleatoire.length; i++) {
+for(let i=0; i<tailleMot; i++) tableauMot[i]=document.getElementById(i);
 
-    }
-});*/
 
-function afficherAlphabet() {
-
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    document.write("<table style=\"margin-top: 25px; background: #232323; width: 100%; height: 200px; display: flex; justify-content: center; flex-wrap: wrap\">") ;
-    document.write("<tr style=\'width: 1100px; height: auto; display: flex; flex-wrap: wrap; justify-content: center; margin-top: 20px\'>") ;
-    for (var i = 0 ; i < alphabet.length ; i++) {
-        document.write("<td style=\" width: 80px; height: 80px; text-align: center\">") ;
-        var lettre = alphabet.charAt(i) ;
-        var touche = "<button style=\"background: #141414; color: white; font-family: 'Arial Black',sans-serif; border: outset 2px darkgrey; border-radius: 5px; width: 70px; height: 70px\" onClick=\"touchCar(this.value)\">" + lettre + "</button>" ;
-        document.write(touche) ;
-        document.write('</td> ') ;
-    }
-    document.write('</tr>') ;
-    document.write("</table>") ;
+function changeCouleur(element,couleur){
+    element.style.backgroundColor=couleur;
 }
 
-afficherAlphabet();
 
-function touchCar(car)
-{
-    var touche = '' ;
-    for (var i = 0 ; i < motsAleatoire.length ; i++) {
-        if (motsAleatoire.charAt(i) !== car ) {
-            touche+= car ;
+function proposer(element){
+    let i;
+    if(element.style.backgroundColor==="lightGreen" ||fini) return;
 
-        }
-        else {
-            touche += reponse.charAt(i) ;
+    const lettre = element.innerHTML;
+    changeCouleur(element,"lightGreen");
 
+    let trouve = false;
+
+    for(i = 0; i < tailleMot; i++) {
+        if(tableauMot[i].innerHTML === lettre) {
+            tableauMot[i].style.visibility='visible';
+            trouve=true;
+            lettresTrouvees++;
         }
     }
-    lettre_trouver.innerHTML = touche ;
-    reponse = touche ;
+
+    if(!trouve){
+        coupsManques++;
+        document.getElementById('perdu_' + coupsManques).style.display = 'block';
+
+        if(coupsManques===9){
+            alert("Vous avez perdu !");
+            for(i = 0; i<tailleMot; i++) tableauMot[i].style.visibility='visible';
+            fini=true;
+        }
+    }
+    if(lettresTrouvees===tailleMot){
+        alert("Bravo ! Vous avez découvert le mot secret !");
+        fini=true;
+    }
 }
 
-
-console.log(motsAleatoire);
